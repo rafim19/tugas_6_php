a35265d7323190bd0d4505b99022dcb3c2317702
Rafi Muhammad <rafi.muhammad001@binus.ac.id>
2021-09-29 13:51:49 +0700

    first commit

---
+++ tugas6.php
@@ -0,0 +1,25 @@
+<?php
+  // Nomor 1
+  $habib = 21;
+  $aji = $habib + 4;
+  $bintan = $aji + 3;
+
+  echo "=================================<br>";
+  echo "Usia Habib = $habib<br>";
+  echo "Usia Aji = $aji<br>";
+  echo "Usia Bintan = $bintan<br>";
+  echo "=================================<br>";
+
+  // Nomor 2
+  $harga_jambu_kg = 15000;
+  $total_jambu = 30;
+  $harga_dus = 2000;
+  $total_dus = 6;
+  $harga_total = ($harga_jambu_kg * $total_jambu) + ($harga_dus * $total_dus);
+
+  echo "=================================<br>";
+  echo "Harga Jambu = Rp $harga_jambu_kg / Kg<br>";
+  echo "Harga Kardus = Rp $harga_dus / pcs<br>";
+  echo "Total Penjualan (Dus dan Jambu) = Rp $harga_total<br>";
+  echo "=================================<br>";
+?>
